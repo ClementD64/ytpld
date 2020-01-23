a89c5bd37cb3624532b72d803e30a3a6a8943c13
Clément Dubreuil <clement.dub60@gmail.com>
2020-01-23 12:41:55 +0100

    fix async

--- src/cutter.js
+++ src/cutter.js
@@ -8,7 +8,7 @@ class Cutter {
         this.end = end;
     }
 
-    cut() {
+    async cut() {
         await exec(`ffmpeg -i ${this.filename} -ss ${this.start} -to ${this.end} -acodec copy ${this.filename}.tmp`);
         await fs.promises.unlink(this.filename);
         await fs.promises.rename(`${this.filename}.tmp`, this.filename);
